2f1f98fa67833ef979f5d1eb7805dfc23c2d8c80
JDode <2420498526@qq.com>
2019-03-28 16:50:29 +0800

    🍻获取jar运行时的命令行command参数😋

--- src/main/java/com/github/JDode/JTesseract/JTesseract.java
+++ src/main/java/com/github/JDode/JTesseract/JTesseract.java
@@ -1,5 +1,7 @@
 package com.github.JDode.JTesseract;
 
+import java.io.File;
+
 /**
  * Created by Ding on 2019/3/28 4:06 PM.
  * <p>
@@ -7,6 +9,9 @@ package com.github.JDode.JTesseract;
  * <a href="https://github.com/JDode">to see GitHub</a>
  */
 public class JTesseract {
+
+
+    private static File config;
 
     // JTesseract LOGO
     private  final String LOGOSTR = "\033[35m" + "\n" +
@@ -22,8 +27,9 @@ public class JTesseract {
     //my github uri
     public  final String GITHUBURI = "https://github.com/JDode/JTesseract";
 
-    public static void main(String[] args) {
-        new JTesseract().init().start();
+
+    public static void main(String[] arg) {
+        new JTesseract().init().start(arg);
 
     }
 
@@ -34,10 +40,14 @@ public class JTesseract {
         System.out.printf("\033[33m  GitHub  : \033[0m %s", this.GITHUBURI);
         return this;
     }
-    private JTesseract start(){
-        System.out.println("\n\033[31m  Message :  配置文件获取异常 --config [file] \033[0m");
-        System.out.printf("\n\033[32m  OCR-Result: %s\033[0m",111);
-        return this;
+    private void start(String[] arg){
+        String[] args = arg;
+        //获取命令行传入的参数
+        if (args[1]==null || args[2]==null || !args[1].equals("--config")){
+            System.out.println("\n\033[31m  Message :  Configuration file gets abnormal （--config [file]） \033[0m");
+        }else {
+            System.out.printf("\n\033[32m  OCR-Result: %s\033[0m",111);
+        }
     }
 }
 
